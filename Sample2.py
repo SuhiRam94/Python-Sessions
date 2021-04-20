90dceb6dadc56c54639e3de0622d244e17f63f11
SuhiRam94 <82896438+SuhiRam94@users.noreply.github.com>
2021-04-20 21:38:53 +0530

    Add files via upload

---
+++ Sample2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+2+3
+
+
+# In[2]:
+
+
+27839+3930303
+
+
+# In[3]:
+
+
+#it is just sample
+
+
+# In[7]:
+
+
+print ("Hello Astronaut")
+
+
+# In[19]:
+
+
+Family = ('Rambabu','Padmavathi','Mounika Rambabu', 'Suhitha Rambabu', 'Rishika Rambabu')
+print(Family)
+
+
+# In[14]:
+
+
+Family = 'Rambabu','Padmavathi','Mounika Rambabu', 'Suhitha Rambabu', 'Rishika Rambabu'
+print (Family)
+
+
+# In[20]:
+
+
+Colors = "Red, Blue, White, Yellow, Black"
+print(Colors)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
